fca481e0aaa22ef552a7bfaed2f1c43a3c981b72
zhan026 <87414517+zhan026@users.noreply.github.com>
2025-06-03 23:02:00 +0500

    Обновил проект: новая версия киноттеатра 1.4

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ db.serialize(() => {
   db.run("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, title TEXT, image TEXT, date TEXT, times TEXT)");
   db.run("CREATE TABLE IF NOT EXISTS bookings (id INTEGER PRIMARY KEY, movie_id INTEGER, seat TEXT, time TEXT)");
 
-  const movies = [[1, 'Дюна 2', 'dune2.jpg', 'сегодня', '13:00,16:00,19:00'], [2, 'Оппенгеймер', 'oppenheimer.jpg', 'сегодня', '12:00,15:30,19:15'], [3, 'Барби', 'barbie.jpg', 'завтра', '11:00,14:00,18:00'], [4, 'Миссия невыполнима', 'mission.jpg', 'послезавтра', '10:00,13:00,17:30'], [5, 'Побег из Шоушенка', 'shawshank.jpg', 'завтра', '09:00,12:30,16:00']];
+  const movies = [
+    [1, "Дюна 2", "dune2.jpg", "сегодня", "13:00,16:00,19:00"],
+    [2, "Оппенгеймер", "oppenheimer.jpg", "сегодня", "12:00,15:30,19:15"],
+    [3, "Барби", "barbie.jpg", "завтра", "11:00,14:00,18:00"],
+    [4, "Миссия невыполнима", "mission.jpg", "послезавтра", "10:00,13:00,17:30"],
+    [5, "Побег из Шоушенка", "shawshank.jpg", "завтра", "09:00,12:30,16:00"]
+  ];
   movies.forEach(([id, title, image, date, times]) => {
     db.run("INSERT OR IGNORE INTO movies (id, title, image, date, times) VALUES (?, ?, ?, ?, ?)", [id, title, image, date, times]);
   });
@@ -24,7 +30,7 @@ app.get('/', (req, res) => {
   const date = req.query.date || 'сегодня';
   db.all("SELECT * FROM movies WHERE date = ?", [date], (err, rows) => {
     const movies = rows.map(m => ({ ...m, times: m.times.split(',') }));
-    res.render('index', { movies });
+    res.render('index', { movies, selectedDate: date });
   });
 });
 
@@ -40,12 +46,6 @@ app.post('/book', (req, res) => {
   db.run("INSERT INTO bookings (movie_id, seat, time) VALUES (?, ?, ?)", [movie_id, seat, time], () => {
     res.send("Бронирование успешно!");
   });
-});
-
-const date = req.query.date || 'сегодня';
-db.all("SELECT * FROM movies WHERE date = ?", [date], (err, rows) => {
-  const movies = rows.map(m => ({ ...m, times: m.times.split(',') }));
-  res.render('index', { movies, selectedDate: date });
 });
 
 const PORT = process.env.PORT || 3000;
